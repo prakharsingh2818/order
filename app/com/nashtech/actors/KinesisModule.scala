b3f7b02a46397a6ddde0792f9c05451b2305b699
jitendra2329 <jitendra.kumar@nashtechglobal.com>
2023-12-12 11:01:22 +0530

    kinesis module added

--- app/com/nashtech/actors/KinesisModule.scala
+++ app/com/nashtech/actors/KinesisModule.scala
@@ -1,10 +1,14 @@
 package com.nashtech.actors
 
 import com.google.inject.AbstractModule
+import com.nashtech.{OrderEventConsumer, OrderEventProcessor, OrderEventProcessorFactory}
 import com.nashtech.services.KinesisConsumerService
 
 class KinesisModule extends AbstractModule {
   override def configure(): Unit = {
+    bind(classOf[OrderEventProcessorFactory]).asEagerSingleton()
+    bind(classOf[OrderEventProcessor]).asEagerSingleton()
+    bind(classOf[OrderEventConsumer]).asEagerSingleton()
     bind(classOf[KinesisConsumerService]).asEagerSingleton()
   }
 }
